3913a39fcec867c7f70838d51bba7f4c78965a41
Sansii <sanbuddhacharyas>
2019-05-12 07:32:48 +0545

    Just after decoding all packet only sending code

---
+++ source_code/os_test.py
@@ -0,0 +1,7 @@
+import os
+mycmd= 'sudo bluetoothctl'
+os.system(mycmd)
+myc = 'microsoft123'
+os.system(myc)
+mycm = 'select-attribute /org/bluez/hci0/dev_03_B3_EC_9A_97_B9/service0016/char0017'
+os.system(mycm)
